2bba15c93be22994ad2a011eff21a9fd4ab2cdb5
ISAAC.LU <t104306033@ntut.org.tw>
2022-08-07 13:50:28 +0800

    Update main.go, create home page using html

--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
 
-import ( // 套件引入可以直接用括號包起來，看起來很舒服
-	"fmt"
+import (
 	"log"
 	"net/http"
-	"strings"
+	"os/exec"
+	"runtime"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) { // 看起來func的需要傳入http讀寫器，http.Request用了*不知道是什麼意思
-	// 解析參數，預設不會解析
-	r.ParseForm() // Q：留個疑問，不解析會怎樣？
+func open(url string) error { // open url from browser
+	var cmd string
+	var args []string
 
-	//這些資訊是輸出到Server的
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
 	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
 
-	fmt.Fprintf(w, "Hello ISAAC!") // 這個寫入到 w 的是輸出到客戶端的
+func homePage(w http.ResponseWriter, _ *http.Request) {
+	var homePageHTML string
+	homePageHTML = `<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<body>
+
+<!-- Content will go here -->
+
+<h1>Hello ISAAC!</h1>
+
+</body>
+</html>
+`
+
+	w.Write([]byte(homePageHTML))
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)     // 設定路由與處理func
-	err := http.ListenAndServe(":9453", nil) // 設定監聽的port
+	http.HandleFunc("/", homePage)
+
+	open("http://127.0.0.1:9453")
+
+	err := http.ListenAndServe(":9453", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err) // Q：這裏報錯不知道會記錄到哪裏去？
+		log.Fatal("ListenAndServe: ", err)
 	}
+
 }
